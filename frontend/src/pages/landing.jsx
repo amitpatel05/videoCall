285ec3719cdb9710f6ecfcad38942924b4109306
amitpatel <amitpatel6671@gmail.com>
2026-03-02 19:23:34 +0530

    Initial files

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "../App.css";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function LandingPage() {
+  const router = useNavigate();
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>Video Call</h2>
+        </div>
+        <div className="navlist">
+          <p
+            onClick={() => {
+              router("/e55rh");
+            }}
+          >
+            Join as Guest
+          </p>
+          <p
+            onClick={() => {
+              router("/auth");
+            }}
+          >
+            Register
+          </p>
+          <div
+            onClick={() => {
+              router("/auth");
+            }}
+            role="button"
+          >
+            Login
+          </div>
+        </div>
+      </nav>
+      <div className="landingMainContainer">
+        <div>
+          <h1>
+            <span style={{ color: "#ff9839" }}>Connect</span> with your loved
+            Ones
+          </h1>
+          <p>Cover a distance by Video Call</p>
+          <div role="button">
+            <Link to={"/auth"}>Get Started</Link>
+          </div>
+        </div>
+        <div>
+          <img src="/mobile.png" alt="mobiles" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/utils/withAuth.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// eslint-disable-next-line no-unused-vars
+const withAuth = (WrappedComponent) => {
+  const AuthComponent = (props) => {
+    const router = useNavigate();
+
+    const isAuthenticated = () => {
+      if (localStorage.getItem("token")) {
+        return true;
+      }
+      return false;
+    };
+
+    useEffect(() => {
+      if (!isAuthenticated()) {
+        router("/auth");
+      }
+    }, []);
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return AuthComponent;
+};
+
+export default withAuth;

a03f4312d6b2a6fb238d690d0e8aa22f32c9184a
amitpatel <amitpatel6671@gmail.com>
2026-03-02 20:09:34 +0530

    Deploy Backend again

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ? "https://videocall-iyrj.onrender.com"
+  ? "https://videocallbackend-pbyp.onrender.com"
   : "http://localhost:8080";
 
 export default server;

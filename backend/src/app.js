d58ab2cca931afab5f8c92d24b2b5b08b6f39476
amitpatel <amitpatel6671@gmail.com>
2026-03-04 17:25:52 +0530

    Change core code

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(
   }),
 );
 
-app.options("*", cors());
+app.options("/*", cors());
 
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
